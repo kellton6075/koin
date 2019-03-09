959695a211995fafcd3db8911fcf4f03007b9417
shubham1501 <shubham.agarwal1@kelltontech.com>
2019-03-09 06:53:33 +0530

    changes

--- src/app/send-koins/send-koins.component.ts
+++ src/app/send-koins/send-koins.component.ts
@@ -9,20 +9,39 @@ import { CommonService } from './../common.service';
 export class SendKoinsComponent implements OnInit {
   address;
   amount;
+  walletBalance;
+
   constructor(private commonService: CommonService) { }
 
   ngOnInit() {
+    this.getUserWallet();
   }
 
   sendkoins(): void {
     if (this.amount && this.address) {
-      this.commonService.sendKoin(this.address, this.amount).subscribe(
-        res => {
-          alert('Transaction successful');
-        }, error => {
-  
-        }
-      )
+      if (this.amount > this.walletBalance) {
+        window.alert('insufficient koins');
+      } else {
+        this.commonService.sendKoin(this.address, this.amount).subscribe(
+          res => {
+            alert('Transaction successful');
+          }, error => {
+    
+          }
+        )
+      }
     }
+  }
+
+  getUserWallet(): void {
+    this.commonService.getUserData().subscribe(
+      res => {
+        this.walletBalance = res.data.coin;
+        console.log(this.walletBalance);
+        
+      }, error => {
+
+      }
+    )
   }
 }
